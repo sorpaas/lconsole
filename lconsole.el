f594341b22aab329a8e821d54bd1e3c8bbc24e57
sorpa'as plat <sorpaas@gmail.com>
2014-04-26 12:34:33 +0800

    add main lconsole-mode function

--- lconsole.el
+++ lconsole.el
@@ -4,4 +4,12 @@
     map)
   "Keymap for lconsole major mode")
 
+(defun lconsole-mode ()
+  "Major mode for lconsole, a Lisp Console."
+  (interactive)
+  (use-local-map lconsole-mode-map)
+  (setq major-mode 'lconsole-mode)
+  (setq mode-name "lconsole")
+  (run-hooks 'lconsole-mode-hook))
 
+(provide 'lconsole-mode)
